5e0a1303a6c0572fa40c461ab3c0c8874f65d2c1
Jason Lo <lcmjlo@gmail.com>
2025-11-01 10:01:08 -0500

    under clock tx

--- transmitter/code.py
+++ transmitter/code.py
@@ -3,6 +3,7 @@ import time
 import adafruit_bno055
 import analogio
 import board
+import microcontroller
 import wifi
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
@@ -28,34 +29,37 @@ def _coalesce_tuple(t, length):
 def _fmt(v: float) -> str:
     """Format a number compactly with 2 decimal places."""
     # Avoid try/except for speed; assume sensors return valid floats
-    return f"{v:.2f}" if isinstance(v, (float, int)) else "nan"
+    return f"{v:.4f}" if isinstance(v, (float, int)) else "nan"
 
 
 # Sensor Initialization
 wifi.radio.enabled = False
+microcontroller.cpu.frequency = 120_000_000
+
+ble = BLERadio()
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
+ble.start_advertising(advertisement)
 print("Starting BLE advertisement...")
-i2c = board.I2C()
 
+# Sensor
+i2c = board.I2C()
 bno055 = adafruit_bno055.BNO055_I2C(i2c)
 bno055.mode = adafruit_bno055.ACCONLY_MODE
 bno055.accel_mode = adafruit_bno055.ACCEL_LOWPOWER1_MODE
 bno055.accel_bandwidth = adafruit_bno055.ACCEL_15_63HZ
-
 battery_voltage = analogio.AnalogIn(board.A0)
-ble = BLERadio()
-ble.start_advertising(advertisement)
 
 while True:
-    if ble.connected:
-        voltage = (battery_voltage.value * 3.3 / 65535) * 2
-        acceleration = _coalesce_tuple(bno055.acceleration, 3)
-        sensors = (voltage,) + acceleration
-        line = ",".join([_fmt(v) for v in sensors]) + "\n"
-        uart.write(line.encode("utf-8"))
-        time.sleep(1)
-    else:
+    if not ble.connected:
         if not ble.advertising:
             ble.start_advertising(advertisement)
-        time.sleep(1)
+        time.sleep(0.2)
+        continue
+
+    voltage = (battery_voltage.value * 3.3 / 65535) * 2
+    acceleration = _coalesce_tuple(bno055.acceleration, 3)
+    sensors = (voltage,) + acceleration
+    line = ",".join([_fmt(v) for v in sensors]) + "\n"
+    uart.write(line.encode("utf-8"))
+    time.sleep(1)
